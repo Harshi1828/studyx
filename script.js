56a6cc6ac81b04f952388126702a4adfc2d86304
Harshi1828 <swey2727@gmail.com>
2026-06-23 16:05:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// JavaScript for ReadNest
+function searchStudy() {
+    let subject = document.getElementById("searchBox").value.toLowerCase();
+    let result = document.getElementById("result");
+
+    if(subject === "programming"){
+        result.innerHTML = "Programming materials available!";
+    }
+    else if(subject === "datascience"){
+        result.innerHTML = "Data Science materials available!";
+    }
+    else if(subject === "database"){
+        result.innerHTML = "Database materials available!";
+    }
+    else{
+        result.innerHTML = "No materials found. Try programming, datascience or database.";
+    }
+}
